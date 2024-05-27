e857269ce2645cbe6128b453099a1ac9c3faf5f9
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-05-27 20:28:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,39 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+using namespace std;
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<string> Tree;
+ifstream file(filename);
+string str = "";
+if (!file) {
+    cout << "opening error" << endl;
+    return Tree;
+}
+while (!file.eof()) {
+    char sym = file.get();
+    str += sym;
+}
+for (char& c : str) {
+    c = tolower(c);
+}
+int len = str.length();
+bool inWord = false;
+string tempStr = "";
+for (int i = 0; i < len; i++) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+        inWord = true;
+    } else {
+        inWord = false;
+    }
+    if (inWord) {
+        tempStr += str[i];
+    } else {
+        Tree.add(tempStr);
+        tempStr = "";
+    }
+}
+file.close();
+return Tree;
 }
