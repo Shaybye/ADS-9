ba231e2dbe2cffd7327afa2974f409809f2ad701
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-05-27 20:40:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,36 +6,36 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-BST<std::string> Tree;
-std::ifstream file(filename);
-std::string strr = "";
-if (!file) {
-    std::cout << "opening error" << std::endl;
+    BST<std::string> Tree;
+    std::ifstream file(filename);
+    std::string strr = "";
+    if (!file) {
+        std::cout << "opening error" << std::endl;
+        return Tree;
+    }
+    while (!file.eof()) {
+        char sym = file.get();
+        strr += sym;
+    }
+    for (char& c : strr) {
+        c = tolower(c);
+    }
+    int len = strr.length();
+    bool inWord = false;
+    std::string tempStrr = "";
+    for (int i = 0; i < len; i++) {
+        if (strr[i] >= 'a' && strr[i] <= 'z') {
+            inWord = true;
+        } else {
+            inWord = false;
+        }
+        if (inWord) {
+            tempStrr += strr[i];
+        } else {
+            Tree.add(tempStrr);
+            tempStrr = "";
+        }
+    }
+    file.close();
     return Tree;
-}
-while (!file.eof()) {
-    char sym = file.get();
-    strr += sym;
-}
-for (char& c : strr) {
-    c = tolower(c);
-}
-int len = strr.length();
-bool inWord = false;
-std::string tempStrr = "";
-for (int i = 0; i < len; i++) {
-    if (strr[i] >= 'a' && strr[i] <= 'z') {
-        inWord = true;
-    } else {
-        inWord = false;
-    }
-    if (inWord) {
-        tempStrr += strr[i];
-    } else {
-        Tree.add(tempStrr);
-        tempStrr = "";
-    }
-}
-file.close();
-return Tree;
 }
